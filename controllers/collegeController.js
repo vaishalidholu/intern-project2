764713771fe9f481618ff21dcadccbae81a1e6e1
vaishalidholu <dholuvaishali97@gmail.com>
2022-09-27 17:17:42 +0530

    get details of college and intern

--- controllers/collegeController.js
+++ controllers/collegeController.js
@@ -1,4 +1,5 @@
 const collegeModel = require("../models/collegeModel");
+const internModel = require("../models/internModel");
 const { isValid, validName } = require("../validation/validate");
 
 const creatCollege = async function (req, res) {
@@ -54,4 +55,126 @@ const creatCollege = async function (req, res) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
-module.exports = { creatCollege };
+
+const getDetails = async function (req, res) {
+  try {
+    console.log(req.query);
+    if (req.body.name)
+      return res.status(400).send({
+        status: false,
+        message: "Please send the college name in query params",
+      });
+
+    if (!req.query.name)
+      return res
+        .status(400)
+        .send({ status: false, message: "Enter collegeName in the query" });
+
+    if (!validName(req.query.name))
+      return res
+        .status(400)
+        .send({ status: false, message: "Enter collegeName in the query" });
+
+    // let FindCollege = await collegeModel
+    //   .findOne({
+    //     name: req.query.name,
+    //     isDeleted: false,
+    //   })
+    //   .lean();
+    // console.log("c", FindCollege);
+
+    // if (!FindCollege)
+    //   return res
+    //     .status(404)
+    //     .send({ status: false, message: "College is not found" });
+
+    // let id = FindCollege._id;
+    // console.log(id);
+
+    // let getIntern = await internModel
+    //   .find({ collegeId: id, isDeleted: false })
+    //   .select({ __v: 0, isDeleted: 0, collegeId: 0 });
+
+    // if (!getIntern.length)
+    //   return res.status(404).send({
+    //     status: false,
+    //     message: `No intern present for ${req.query.name} college`,
+    //   });
+
+    // FindCollege["intern"] = getIntern;
+    // console.log(FindCollege);
+    // return res.status(200).send({
+    //   status: true,
+    //   data: FindCollege,
+    // });
+
+    let agg = await collegeModel.aggregate([
+      {
+        $match: {
+          name: req.query.name,
+        },
+      },
+      {
+        $lookup: {
+          from: "interns",
+          localField: "_id",
+          foreignField: "collegeId",
+          as: "result",
+        },
+      },
+      {
+        $project: {
+          name: 1,
+          fullName: 1,
+          isDeleted: 1,
+          "result._id": 1,
+          "result.name": 1,
+          "result.email": 1,
+          "result.mobile": 1,
+        },
+      },
+      {
+        $unwind: {
+          path: "$result",
+        },
+      },
+    ]);
+
+    return res.status(200).send({
+      status: true,
+      data: agg,
+    });
+
+    // [
+    //   {
+    //     '$match': {
+    //       'name': 'gec'
+    //     }
+    //   }, {
+    //     '$lookup': {
+    //       'from': 'interns',
+    //       'localField': '_id',
+    //       'foreignField': 'collegeId',
+    //       'as': 'result'
+    //     }
+    //   }, {
+    //     '$project': {
+    //       'name': 1,
+    //       'fullName': 1,
+    //       'isDeleted': 1,
+    //       'result._id': 1,
+    //       'result.name': 1,
+    //       'result.email': 1,
+    //       'result.mobile': 1
+    //     }
+    //   }, {
+    //     '$unwind': {
+    //       'path': '$result'
+    //     }
+    //   }
+    // ]
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.message });
+  }
+};
+module.exports = { creatCollege, getDetails };
--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 // const { creatCollege } = require("../c/collegeController");
-const { creatCollege } = require("../controllers/collegeController");
+const {
+  creatCollege,
+  getDetails,
+} = require("../controllers/collegeController");
 const { creatIntern } = require("../controllers/internController");
 const router = express.Router();
 
 router.post("/creatCollege", creatCollege);
 router.post("/creatIntern", creatIntern);
+router.get("/getDetails", getDetails);
 module.exports = router;
